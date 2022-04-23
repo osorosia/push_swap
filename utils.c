0b4ac2175d986802f6d37b243de3ce3ff8019d01
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-24 07:25:58 +0900

    norm

--- utils.c
+++ utils.c
@@ -1,27 +1,31 @@
 #include "push_swap.h"
 
-void error(char *str) {
+void	error(char *str)
+{
 	ft_putstr_fd(str, 2);
 	ft_putstr_fd("\n", 2);
 	exit(1);
 }
 
-long atoi_or_err(char *str) {
-	long num;
-	long sign;
-	long i;
+long	atoi_or_err(char *str)
+{
+	long	num;
+	long	sign;
+	long	i;
 
 	num = 0;
 	sign = 1;
 	i = 0;
-	if (str[i] == '-' || str[i] == '+') {
+	if (str[i] == '-' || str[i] == '+')
+	{
 		if (str[i] == '-')
 			sign = -1;
 		i++;
 	}
 	if (str[i] == '\0')
 		error("Error");
-	while(str[i] != '\0') {
+	while (str[i] != '\0')
+	{
 		if (!ft_isdigit(str[i]))
 			error("Error");
 		num = num * 10 + str[i] - '0';
@@ -29,5 +33,5 @@ long atoi_or_err(char *str) {
 			error("Error");
 		i++;
 	}
-	return sign * num;
+	return (sign * num);
 }
