db20eda6df9c1128a72197fd876419faa95322e3
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-24 07:42:03 +0900

    norm

--- numbers.c
+++ numbers.c
@@ -1,16 +1,18 @@
 #include "push_swap.h"
 
-char **create_number_strs(int argc, char **argv) {
-	char **numbers;
-	char **strs;
-	long i;
-	long j;
-	long index;
+char	**create_number_strs(int argc, char **argv)
+{
+	char	**numbers;
+	char	**strs;
+	long	i;
+	long	j;
+	long	index;
 
 	numbers = ft_calloc(MAX_NUMBERS_LEN + 1, sizeof(char *));
 	i = 1;
 	index = 0;
-	while (i < argc) {
+	while (i < argc)
+	{
 		strs = ft_split(argv[i], ' ');
 		if (strs == NULL)
 			error("malloc error");
@@ -22,11 +24,12 @@ char **create_number_strs(int argc, char **argv) {
 		free(strs);
 		i++;
 	}
-	return numbers;
+	return (numbers);
 }
 
-void free_number_strs(char **numbers) {
-	long i;
+void	free_number_strs(char **numbers)
+{
+	long	i;
 
 	i = 0;
 	while (numbers[i])
