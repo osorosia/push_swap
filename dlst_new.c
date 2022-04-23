f25f3343f8e384e297b4be96d72ecc6f9898beca
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-24 07:37:18 +0900

    norm

--- dlst_new.c
+++ dlst_new.c
@@ -1,22 +1,28 @@
 #include "push_swap.h"
 
-t_dlst *dlst_new_dummy() {
-	t_dlst *dlst = ft_calloc(1, sizeof(t_dlst));
+t_dlst	*dlst_new_dummy(void)
+{
+	t_dlst	*dlst;
+
+	dlst = ft_calloc(1, sizeof(t_dlst));
 	if (dlst == NULL)
 		error("malloc error");
 	dlst->prev = dlst;
 	dlst->next = dlst;
 	dlst->is_dummy = true;
-	return dlst;
+	return (dlst);
 }
 
-t_dlst *dlst_new(t_dlst *prev, t_dlst *next, long num) {
-	t_dlst *dlst = ft_calloc(1, sizeof(t_dlst));
+t_dlst	*dlst_new(t_dlst *prev, t_dlst *next, long num)
+{
+	t_dlst	*dlst;
+
+	dlst = ft_calloc(1, sizeof(t_dlst));
 	if (dlst == NULL)
 		error("malloc error");
 	dlst->prev = prev;
 	dlst->next = next;
 	dlst->num = num;
 	dlst->is_dummy = false;
-	return dlst;
+	return (dlst);
 }
