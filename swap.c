69b13b132dcd44dffbc98751c67bc83ea205229f
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-24 07:35:36 +0900

    norm

--- swap.c
+++ swap.c
@@ -1,32 +1,43 @@
 #include "push_swap.h"
 
-void sa(t_dlst *a, t_dlst *b, bool flag) {
+void	sa(t_dlst *a, t_dlst *b, bool flag)
+{
+	t_dlst	*first;
+	t_dlst	*second;
+	long	tmp;
+
 	(void)b;
 	if (a->next->next->is_dummy)
-		return;
-	t_dlst *first = a->next;
-	t_dlst *second = first->next;
-	long tmp = first->num;
+		return ;
+	first = a->next;
+	second = first->next;
+	tmp = first->num;
 	first->num = second->num;
 	second->num = tmp;
 	if (flag)
 		ft_putstr_fd("sa\n", 1);
 }
 
-void sb(t_dlst *a, t_dlst *b, bool flag) {
+void	sb(t_dlst *a, t_dlst *b, bool flag)
+{
+	t_dlst	*first;
+	t_dlst	*second;
+	long	tmp;
+
 	(void)a;
 	if (b->next->next->is_dummy)
-		return;
-	t_dlst *first = b->next;
-	t_dlst *second = first->next;
-	long tmp = first->num;
+		return ;
+	first = b->next;
+	second = first->next;
+	tmp = first->num;
 	first->num = second->num;
 	second->num = tmp;
 	if (flag)
 		ft_putstr_fd("sb\n", 1);
 }
 
-void ss(t_dlst *a, t_dlst *b, bool flag) {
+void	ss(t_dlst *a, t_dlst *b, bool flag)
+{
 	sa(a, b, false);
 	sb(a, b, false);
 	if (flag)
