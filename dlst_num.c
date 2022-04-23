b71d8240371232498195dcbae992dde4dfcbb6c1
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-24 07:39:50 +0900

    norm

--- dlst_num.c
+++ dlst_num.c
@@ -1,47 +1,57 @@
 #include "push_swap.h"
 
-long dlst_front_num(t_dlst *dummy, long index) {
+long	dlst_front_num(t_dlst *dummy, long index)
+{
 	if (dlst_size(dummy) < index + 1)
 		error("error: dlst_front_num()");
-	dummy = dummy->next; 
-	while (index > 0) {
+	dummy = dummy->next;
+	while (index > 0)
+	{
 		dummy = dummy->next;
 		index--;
 	}
-	return dummy->num;
+	return (dummy->num);
 }
 
-long dlst_back_num(t_dlst *dummy, long index) {
+long	dlst_back_num(t_dlst *dummy, long index)
+{
 	if (dlst_size(dummy) < index + 1)
 		error("error: dlst_back_num()");
 	dummy = dummy->prev;
-	while (index > 0) {
+	while (index > 0)
+	{
 		dummy = dummy->prev;
 		index--;
 	}
-	return dummy->num;
+	return (dummy->num);
 }
 
-long get_min_in_dlst(t_dlst *dummy, long border) {
-	long min = (long)INT_MAX + 1;
+long	get_min_in_dlst(t_dlst *dummy, long border)
+{
+	long	min;
 
+	min = (long)INT_MAX + 1;
 	dummy = dummy->next;
-	while (!dummy->is_dummy) {
+	while (!dummy->is_dummy)
+	{
 		if (dummy->num >= border && dummy->num < min)
 			min = dummy->num;
 		dummy = dummy->next;
 	}
-	return min;
+	return (min);
 }
 
-long get_max_in_dlst(t_dlst *dummy, long border) {
-	long max = (long)INT_MIN - 1;
+long	get_max_in_dlst(t_dlst *dummy, long border)
+{
+	long	max;
 
+	max = (long)INT_MIN - 1;
 	dummy = dummy->next;
-	while (!dummy->is_dummy) {
+	while (!dummy->is_dummy)
+	{
 		if (dummy->num <= border && dummy->num > max)
 			max = dummy->num;
 		dummy = dummy->next;
 	}
-	return max;
+	return (max);
 }
