1af49fc77b627eae62bcc38aad5dca0c800d5421
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-24 07:28:44 +0900

    norm

--- dlst_compression.c
+++ dlst_compression.c
@@ -1,24 +1,34 @@
 #include "push_swap.h"
 
-t_dlst *get_min_dlst_without_compressed(t_dlst *a) {
-	long min = (long)INT_MAX + 1;
-	t_dlst *dlst = NULL;
+t_dlst	*get_min_dlst_without_compressed(t_dlst *a)
+{
+	long	min;
+	t_dlst	*dlst;
 
+	min = (long)INT_MAX + 1;
+	dlst = NULL;
 	a = a->next;
-	while (!a->is_dummy) {
-		if (!a->is_compression && (!dlst || min > a->num)) {
+	while (!a->is_dummy)
+	{
+		if (!a->is_compression && (!dlst || min > a->num))
+		{
 			min = a->num;
 			dlst = a;
 		}
 		a = a->next;
 	}
-	return dlst;
+	return (dlst);
 }
 
-void dlst_compression(t_dlst *a) {
-	long i = 0;
-	while (i < dlst_size(a)) {
-		t_dlst *min = get_min_dlst_without_compressed(a);
+void	dlst_compression(t_dlst *a)
+{
+	long	i;
+	t_dlst	*min;
+
+	i = 0;
+	while (i < dlst_size(a))
+	{
+		min = get_min_dlst_without_compressed(a);
 		min->num = i;
 		min->is_compression = true;
 		i++;
